d9f74e06d7993460818e295975cff4f00ef18820
ritikkumar352 <krritik2002@gmail.com>
2025-03-03 23:17:08 +0530

    Project setup,Initial commit

---
+++ chatLive/src/main/java/com/chat/chatLive/ChatLiveApplication.java
@@ -0,0 +1,14 @@
+package com.chat.chatLive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatLiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatLiveApplication.class, args);
+		System.out.println("Started !! now complete this.. till SUNDAY");
+	}
+
+}
---
+++ chatLive/src/test/java/com/chat/chatLive/ChatLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.chatLive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatLiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
